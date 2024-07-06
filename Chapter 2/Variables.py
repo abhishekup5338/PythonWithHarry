b73f1e05f41f6f7e86a69b7085bc03c8e1aab903
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-07 03:50:36 +0530

    ch 2

---
+++ Chapter 2/Variables.py
@@ -0,0 +1,6 @@
+
+# A variable is the name given to a memory location in a program.
+
+a = 30  #variables = container to store a value
+b = "abhi"  #keywords = reserved words in python
+c = 71.22   #identifiers = class/function/variable name
