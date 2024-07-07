82e4775555153512f7ffbc8a9c2312f3e71f70a9
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-08 01:53:03 +0530

    ch 2

--- Chapter 2/06_Input.py
+++ Chapter 2/06_Input.py
@@ -1,10 +1,12 @@
 
 
-# To replicate the above line 'ALT+SHIFT+DOWN ARROW KEY'
+# # To replicate the above line 'ALT+SHIFT+DOWN ARROW KEY'
+# The input value is by default take string we need to convert them
 
-a = input("Enter number 1 ")
 
-b = input("Enter number 2 ")
+a = int(input("Enter number 1 "))
+
+b = int(input("Enter number 2 "))
 
 print("Number a is :", a)
 print("Number b is :", b)
