ab1e11611374c563a99e130153a977d410d36a72
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-08 01:49:50 +0530

    ch 2

---
+++ Chapter 2/06_Input.py
@@ -0,0 +1,11 @@
+
+
+# To replicate the above line 'ALT+SHIFT+DOWN ARROW KEY'
+
+a = input("Enter number 1 ")
+
+b = input("Enter number 2 ")
+
+print("Number a is :", a)
+print("Number b is :", b)
+print("Sum a or b is :", a + b)
