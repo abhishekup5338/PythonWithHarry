f4e60e052cdfdb2a31845ccf456c8884f5027211
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-08 01:00:41 +0530

    ch 2

--- Chapter 2/01_Variables.py
+++ Chapter 2/01_Variables.py
@@ -4,3 +4,7 @@
 a = 30  #variables = container to store a value
 b = "abhi"  #keywords = reserved words in python
 c = 71.22   #identifiers = class/function/variable name
+
+x = 2
+y = 5
+print(x + y)
---
+++ Chapter 2/02_Datatype.py
@@ -0,0 +1,10 @@
+
+a = 2 #a is an integer
+
+b = 5.22 # b is a floating point number
+
+c = "abhi" # c is a string
+
+d = False # d is an boolean variable
+
+e = None # e is a none type variable
