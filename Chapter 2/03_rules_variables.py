2ab16e91b56e8f2fabaf5d33d49eda0fd249d4cf
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-08 01:30:26 +0530

    ch 2

--- Chapter 2/03_rules_variables.py
+++ Chapter 2/03_rules_variables.py
@@ -1,4 +1,6 @@
+# For the details rules for variable refer the python handbook by harry
 
+# Reason behind this rules created is there are some variables which python interpretor used to define some function
 a = 23
 
 aaa = 376
---
+++ Chapter 2/04_Operators.py
@@ -0,0 +1,40 @@
+
+#Arithmatic Operators
+
+a = 7
+
+b = 4
+c = a + b
+print(c)
+
+#Assignment Operators
+
+a = 4-2 #assign 4-2 in a
+print(a)
+b = 6 
+b += 3 #Increment the val of b by 3 and then assign it to b 
+# b -= 3 #Decrement the val of b by 3 and then assign it to b 
+# b *= 3 #Multiply the val of b by 3 and then assign it to b 
+# b /= 3 #Divide the val of b by 3 and then assign it to b 
+print(b)
+
+
+# #Comparision Operators
+# It will return true/false every time 
+d = 5<4
+print(d)
+
+
+#  Logical Operators
+
+e = True or False #Truth table of 'OR'
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+#Truth table of 'AND'
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
