6589bbdddd2a0b588576ac2db9c09ba01a980d07
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-11 00:56:18 +0530

    ch 2 ps

---
+++ Chapter 2/Chapter 2 - PS/Problem_5.py
@@ -0,0 +1,7 @@
+
+# Write a program to find the average of 2 number
+
+a = int(input("Enter number a : "))
+b = int(input("Enter number b : "))
+
+print("The average of this two number is :",(a+b)/2)
