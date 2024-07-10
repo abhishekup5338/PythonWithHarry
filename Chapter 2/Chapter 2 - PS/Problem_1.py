b7a6190317db7b78107ac04c15e24b1443dbfd71
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-10 20:06:19 +0530

    ch 2 PS

---
+++ Chapter 2/Chapter 2 - PS/Problem_1.py
@@ -0,0 +1,4 @@
+# Write a python program to add 2 numbers
+a = 5
+b = 3
+print(a+b)
---
+++ Chapter 2/Chapter 2 - PS/Problem_2.py
@@ -0,0 +1,7 @@
+# write a python program to find remainder when a number is divided by z
+
+a = 38
+
+b = 4
+
+print("Remainder when a number is divided by any number : ", a % b)
