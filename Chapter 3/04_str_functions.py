04849be7c05bbba27dcf2c6aea48d02aa53830f7
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-11 21:21:35 +0530

    ch 3

---
+++ Chapter 3/04_str_functions.py
@@ -0,0 +1,18 @@
+#Python functions are case sensitive
+name = "Harry"
+
+print(len(name))
+
+print(name.endswith("rry"))
+
+print(name.endswith("rrya"))
+
+print(name.startswith("Ha"))
+
+print(name.capitalize())  #This function will only capitalize the first letter
+
+print(name.lower())
+
+print(name.title())
+
+print(name.upper())
