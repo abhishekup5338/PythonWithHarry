aa62da32bea9310461c42bea6e413db3a92923f8
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-13 00:08:52 +0530

    ch 3 ps

---
+++ Chapter 3/Chapter 3 - PS/Problem_4.py
@@ -0,0 +1,7 @@
+#Replace double space with single space
+
+name = "ABHFL PROD  SERVER LOGIN"
+
+print(name.replace("  "," "))
+
+print(name) #Strings are immutable which means that you cannot change them by running function on them
