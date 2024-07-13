6f59e544d9fc399db3b4b5c202bee4d4cf926648
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-14 00:08:41 +0530

    ch 4

---
+++ Chapter 4/01_list.py
@@ -0,0 +1,9 @@
+
+friends = ["Apple", "Orange", 5, 38.39, False, "Akash", "Rohan"]
+
+print(friends[0])
+
+friends[0]= "Grapes"  #Unlike Strings lists are mutable
+print(friends[0])
+
+print(friends[1:4])
