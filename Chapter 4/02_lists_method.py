196c4463c5c624050d070bfad2a7530d7e443626
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-14 00:28:17 +0530

    ch 4

---
+++ Chapter 4/02_lists_method.py
@@ -0,0 +1,14 @@
+friends = ["Apple", "Orange", 5, 38.39, False, "Akash", "Rohan"]
+
+print(friends)
+
+friends.append("Abhi")  #Append means adding any extra value at the end of list.
+print(friends)
+
+l1 = [1,45,39,23,94,4,]
+# l1.sort()
+l1.reverse()
+l1.insert(3,34)  #Insert 34 at 3 index in list
+l1.remove(39)
+
+print(l1)
