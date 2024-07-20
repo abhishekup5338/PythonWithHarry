f962ca6afdf1a11ccba31c32005c4f3ccb29364c
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-21 01:29:29 +0530

    ch 5 5

---
+++ Chapter 5/05_set_union_intersection.py
@@ -0,0 +1,7 @@
+
+s1 = {1, 93, 39, 54}
+s2 = {1, 43, 93, 94}
+
+print(s1.union(s2))  #it will return only unique values
+
+print(s1.intersection(s2)) #It will return only matching values in both the sets
