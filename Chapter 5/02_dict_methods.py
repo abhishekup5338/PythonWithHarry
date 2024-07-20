c69c9f56453df8bab36f9b4bc005183320ff17ae
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-21 00:56:49 +0530

    ch 5 2

---
+++ Chapter 5/02_dict_methods.py
@@ -0,0 +1,14 @@
+
+marks = {
+
+     "Abhi" : 100,
+     "Akhilesh" : 35,
+     "Aji" : 23,
+     0: "Aniket"
+}
+
+# print(marks.items())  #Give key:value pairs in tuple form
+# print(marks.keys())
+# print(marks.values())
+marks.update({"Abhi": 99,"Amit":90}) #It will update current dict because it is mutable.
+print(marks)
