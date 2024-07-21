e37e54dbf3cbdd23925f7566c60d835add28ef57
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-22 01:37:59 +0530

    ch 6 2

--- Chapter 6/01_conditionals.py
+++ Chapter 6/01_conditionals.py
@@ -1,14 +1,11 @@
 
 a = int(input("Enter your age : "))
 
-#if elif else ladder
+#if  else statement
 
 if(a>=18):
     print("You can vote")
-elif(a<0):
-    print("You are entering a invalid negative age")
-elif(a==0):
-    print("You are entering 0 is not a invalid age")
+
 else:
     print("You cannot vote")
 
---
+++ Chapter 6/02_if_elif_else_ladder.py
@@ -0,0 +1,17 @@
+
+
+a = int(input("Enter your age : "))
+
+#if elif else ladder
+
+if(a>=18):
+    print("You can vote")
+elif(a<0):
+    print("You are entering a invalid negative age")
+elif(a==0):
+    print("You are entering 0 is not a invalid age")
+else:
+    print("You cannot vote")
+
+print(a)
+print("Ens of program")
