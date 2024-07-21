04534d0da87ea82656b3a9588e0849ae9edda958
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-21 22:19:49 +0530

    ch 5 ps 6

---
+++ Chapter 5/Chapter 5 - PS/Problem_6.py
@@ -0,0 +1,20 @@
+# Create an empty dict and add 4 friends name and favourte language.
+d = {}
+
+name = input("Enter friend name : ")
+lang = input("Enter language name : ")
+d.update({name : lang})
+
+name = input("Enter friend name : ")
+lang = input("Enter language name : ")
+d.update({name : lang})
+
+name = input("Enter friend name : ")
+lang = input("Enter language name : ")
+d.update({name : lang})
+
+name = input("Enter friend name : ")
+lang = input("Enter language name : ")
+d.update({name : lang})
+
+print(d)
