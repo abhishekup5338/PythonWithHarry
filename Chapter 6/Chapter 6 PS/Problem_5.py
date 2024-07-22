e6fd5ef6295ad4663c5900309fcf0acabf5374ce
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-22 22:13:39 +0530

    ch 6 ps 5

---
+++ Chapter 6/Chapter 6 PS/Problem_5.py
@@ -0,0 +1,9 @@
+
+l = ["abhi", "akki", "sahil", "aniket", "shiv"]
+
+name = input("Enter your name : ")
+
+if(name in l):
+    print("Your name is in list")
+else:
+    print("Your name is not in list")
