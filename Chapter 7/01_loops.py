d68b3a17107ff2f8fe4876c45aa29639528aff83
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-23 22:15:45 +0530

    ch 7 1

---
+++ Chapter 7/01_loops.py
@@ -0,0 +1,12 @@
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+#Using Loops it is more efficient and easy.
+#This program can print like this in loop:
+
+for i in range(1, 12): 
+    print(i)
