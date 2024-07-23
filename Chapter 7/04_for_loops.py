04ee2ffe09bfb871475dfbec72e568426ee22855
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-23 22:40:44 +0530

    ch 7 4

---
+++ Chapter 7/04_for_loops.py
@@ -0,0 +1,8 @@
+
+#A for loop is used to iterate through a sequence like list, tuple, or string[iterable]
+for i in range(5):
+    print(i)
+
+
+for i in range(0, 101, 3):  #Give size of variable
+    print(i)
