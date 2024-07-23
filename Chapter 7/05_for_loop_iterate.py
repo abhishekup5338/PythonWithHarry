6330bbac336979410679a856c6043ef4c80bc2af
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-23 22:47:03 +0530

    ch 7 5

---
+++ Chapter 7/05_for_loop_iterate.py
@@ -0,0 +1,15 @@
+#for loop with list
+l = [1, 39, 392, 303, 32, 2, 301]
+
+for i in l:
+    print(i)
+
+#for loop with tuple
+t = (2, 94, 21, 9, 34)
+for i in t:
+    print(i)
+
+#for loop with string
+s = "Abhishek"
+for i in s:
+    print(i)
