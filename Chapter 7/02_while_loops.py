a8b862377fbf3c8443c62a51b5f3147b7b63ec09
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-07-23 22:33:58 +0530

    ch 7 3

--- Chapter 7/02_while_loops.py
+++ Chapter 7/02_while_loops.py
@@ -4,4 +4,4 @@ i = 1
 while(i<51):
     print(i)
     
-    i+=1 
+    i+=1  #i= i+1
---
+++ Chapter 7/03_list_using_while.py
@@ -0,0 +1,8 @@
+
+list = [2, "Abhi", "Akki", True, 21, "anup", "guru", 32]
+
+i = 0
+
+while(i<len(list)):
+    print(list[i])
+    i = i +1
