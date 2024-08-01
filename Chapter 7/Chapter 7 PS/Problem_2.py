8d83df209a09ca7f638aafd989756ce3d985158d
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-08-01 23:42:34 +0530

    c

--- Chapter 7/Chapter 7 PS/Problem_2.py
+++ Chapter 7/Chapter 7 PS/Problem_2.py
@@ -1,6 +1,6 @@
 #Write a program to greet all the person name store in list 'l' and which start with 'S'.
 
-l = ["Abhi", "Sam", "anup", "Shiv", "Soham", "harry"]
+l = ["Abhi", "Sam", "anup", "Shiv", "Soham", "harry"] #This is list
 
 for name in l: #logic
     if(name.startswith("S")): 
