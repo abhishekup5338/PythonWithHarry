c726f3838cbb1552e684cb68799b3d988a1bb5ac
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-08-14 22:18:54 +0530

    c

---
+++ Chapter 8/01_Functions.py
@@ -0,0 +1,29 @@
+
+#Find the average of number
+
+# a = int(input("Enter a number : "))
+# b= int(input("Enter a number : "))
+# c = int(input("Enter a number : "))
+
+# average = (a+b+c)/3
+# print(average)
+
+#Function Defination
+def avg():
+    a = int(input("Enter a number : "))
+    b= int(input("Enter a number : "))
+    c = int(input("Enter a number : "))
+
+    average = (a+b+c)/3
+    print(average)
+
+avg() #Function Call
+avg()
+print("Thank you")
+avg()
+print("Thank you")
+avg()
+avg()
+print("Thank you")
+avg()
+print("Thank you")
