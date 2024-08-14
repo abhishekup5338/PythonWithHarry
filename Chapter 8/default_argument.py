a310087f10b1320391951943e50dc016b12469a9
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-08-14 22:51:28 +0530

    ch 8 4

---
+++ Chapter 8/default_argument.py
@@ -0,0 +1,6 @@
+
+def goodday(name, ending="Thank you"): #this is default valur of paramenter.
+    print("Good Day, " + name)
+    print(ending)
+    #return "done"
+goodday("Abhishek") #when parameter value is not given it will take default prameter value.
