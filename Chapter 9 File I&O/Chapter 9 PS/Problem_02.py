580f5d3e7513f23f4f8bed79d37a83753793c33c
abhishekup5338 <138136819+abhishekup5338@users.noreply.github.com>
2024-09-28 15:45:02 +0530

    ch 9 ps 2

--- Chapter 9 File I&O/Chapter 9 PS/Problem_02.py
+++ Chapter 9 File I&O/Chapter 9 PS/Problem_02.py
@@ -1,1 +1,21 @@
-.
+import random
+
+def game():
+    print("Your are playing a game...")
+    score = random.randint(1, 65)
+    #Fetch the Hiscore
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore!=""):
+            hiscore=int(hiscore)
+        else:
+            hiscore = 0
+    print(f"Your score : {score}")
+    if(score>hiscore):
+        #write this to the file
+        with open("hiscore.txt", "w") as f:
+            f.write(str(score))
+
+    return score
+
+game()
